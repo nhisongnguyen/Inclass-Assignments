7eb415dbd44a496968df6c58a63724d5182c5cc4
nhinguyen <nhinguyen@localhost>
2022-03-09 21:25:00 -0600

    MyBank Commit

--- assignments/src/main/java/com/assignments/MyBank.java
+++ assignments/src/main/java/com/assignments/MyBank.java
@@ -7,37 +7,41 @@ public class MyBank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("1\t Create Account");
-		System.out.println("2\t Credit");
-		System.out.println("3\t Debit");
-		System.out.println("4\t Transfer Amount");
-		System.out.println("5\t View Balance");
-		System.out.println("6\t Do you want to continue");
-		int key = scan.nextInt();
-		System.out.println("Enter the option: " + key);
-		
-		switch(key){
-		case 1:
+		while(true) {
+			System.out.println("**********Bank Main Menu*********");
 			System.out.println("1\t Create Account");
-			break;
-		case 2:
 			System.out.println("2\t Credit");
-			break;
-		case 3:
 			System.out.println("3\t Debit");
-			break;
-		case 4:
 			System.out.println("4\t Transfer Amount");
-			break;
-		case 5:
 			System.out.println("5\t View Balance");
-			break;
-		case 6:
 			System.out.println("6\t Do you want to continue");
-			break;
-		default: System.out.println("Invalid choice");
-		}
+			System.out.println("Enter your choice: \t");
+			
+			int key = scan.nextInt();
+			//System.out.println("Enter the option: " + key);
+			
+			switch(key){
+			case 1:
+				System.out.println("\t Create Account\n");
+				break;
+			case 2:
+				System.out.println("\t Credit\n");
+				break;
+			case 3:
+				System.out.println("\t Debit\n");
+				break;
+			case 4:
+				System.out.println("\t Transfer Amount\n");
+				break;
+			case 5:
+				System.out.println("\t View Balance\n");
+				break;
+			case 6:
+				System.out.println("\t Do you want to continue\n");
+				break;
+			default: System.out.println("Invalid choice \n");
+			}
 	}
+}
 
 }
